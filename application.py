14a3d4924c69f17f9cc0fe12e5dafbe4075327f4
anapaulafranco <63746048+anapaulafranco@users.noreply.github.com>
2020-12-07 14:08:52 -0300

    Update application.py

--- application.py
+++ application.py
@@ -21,7 +21,7 @@ g = []
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 #api = tweepy.API(auth)
-api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True) 
+api = tweepy.API(auth, wait_on_rate_limit=True) 
 
 @application.route("/")
 def hello():
